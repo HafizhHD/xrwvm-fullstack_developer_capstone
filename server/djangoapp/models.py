5d8d83d246826138df9194d11e3d931f225143a2
HafizhHD <17662247+HafizhHD@users.noreply.github.com>
2024-10-15 04:24:30 -0400

    feat: dealerships

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -43,6 +43,9 @@ class CarModel(models.Model):
     color = models.CharField(max_length=50, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
