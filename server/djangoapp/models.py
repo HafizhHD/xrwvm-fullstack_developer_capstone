4108975e806307ec726881a410fb561c878e65a7
HafizhHD <17662247+HafizhHD@users.noreply.github.com>
2024-10-15 04:58:13 -0400

    feat: pylint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -15,6 +14,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     SEDAN = "Sedan"
@@ -39,9 +39,17 @@ class CarModel(models.Model):
     )
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=20, choices=CAR_TYPES, default="SUV")
-    year = models.IntegerField(default=2024, validators=[MaxValueValidator(2024), MinValueValidator(1950)])
+    year = models.IntegerField(
+        default=2024,
+        validators=[MaxValueValidator(2024), MinValueValidator(1950)]
+    )
     color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
