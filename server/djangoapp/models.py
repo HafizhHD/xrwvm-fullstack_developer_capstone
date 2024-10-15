d00b8e5ff0205862ad873655b59b895ba40f900a
HafizhHD <17662247+HafizhHD@users.noreply.github.com>
2024-10-15 00:18:47 -0400

    feat: cars

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,47 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True, null=True)
+    year_founded = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+    HATCHBACK = "Hatchback"
+    CONVERTIBLE = "Convertible"
+    COUPE = "Coupe"
+    TRUCK = "Truck"
+
+    CAR_TYPES = [
+        ('SEDAN', "Sedan"),
+        ('SUV', "SUV"),
+        ('WAGON', "Wagon"),
+        ('TRUCK', "Truck"),
+        ('CONVERTIBLE', "Convertible"),
+        ('COUPE', "Coupe"),
+    ]
+
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE, related_name="models"
+    )
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(default=2024, validators=[MaxValueValidator(2024), MinValueValidator(1950)])
+    color = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
